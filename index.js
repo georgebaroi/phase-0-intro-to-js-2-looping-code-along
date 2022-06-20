94f18f85c29e4817335a5e8ba0aed291fd51adfb
George Baroi <baroi.george@gmail.com>
2022-06-20 00:34:26 -0400

    commit tree ea42f8396fdbc3431d5d8c3a461ceacd573008d2

--- index.js
+++ index.js
@@ -1,1 +1,18 @@
-// Code your solutions in this file
+
+const recipients = ["Charlie", "Samip", "Ali"]
+let birthday = `Thank you, ${recipients}, for the wonderful gift!`
+function writeCard(recipients , birthday){
+    for( let i =0; i<recipients.length; i++){
+        birthday = `Thank you, ${recipients[i]}, for the wonderful gift!`
+        return recipients, birthday
+    }
+}
+console.log(writeCard(["Charlie", "Samip", "Ali"], birthday));
+debugger
+
+
+    
+      
+
+
+
